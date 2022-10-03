1321ba449b3eaef1b2aef37ef35e8fa509c49676
Zhupf <zhupff@qq.com>
2022-10-03 22:57:22 +0800

    Build scripts framework.

---
+++ buildSrc/src/main/kotlin/desktop-template-script.gradle.kts
@@ -0,0 +1,27 @@
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.project
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(":common"))
+                implementation(compose.desktop.currentOs)
+            }
+        }
+    }
+}
